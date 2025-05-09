67b6da90d51ad69fad52fa6ad99898416f73edcb
Shilpa Vijayan <shilpa.v@webandcrafts.com>
2025-05-09 12:26:54 +0530

    changed the auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,13 @@ import { PassportModule } from '@nestjs/passport';
 import { jwtConstants } from './constants';
 import { UserService } from 'src/user/user.service';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
     PassportModule,
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: jwtConstants.secret, // Or load from env
       signOptions: { expiresIn: '1h' },
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,23 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
-  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService,JwtStrategy],
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn: '1h' },
+    }),
+  }),],
   exports: [UserService],
 })
 export class UserModule {}
